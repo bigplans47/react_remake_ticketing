4596ee88808859a75b3ea60a18baedf7a6d6fc11
Dan Kiss <flowfast47@gmail.com>
2018-01-22 13:16:30 -0800

    add code to practice using eventhandler

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class NewTicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick(){
+    console.log("hey you clicked me, i dont have code to change my state but will soon have code for this");
+  }
+
+  render(){
+    return (
+      <div>
+        <p>This is the NewTicketControl component!</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
+      </div>
+    );
+  }
+}
+
+export default NewTicketControl;
